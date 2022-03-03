68ee1fb3ebd14b351d4f8fb917b92695acc6913a
Mike Morris <michael.a.morris.public@gmail.com>
2022-03-02 21:30:12 -0600

    Finished Gatsby Tutorial Part 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage
+        src='../images/clifford.webp'
+        alt='Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera'
+      />
     </Layout>
   )
 }

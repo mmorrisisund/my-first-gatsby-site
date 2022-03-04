4803f142ec7bfc3fd8ed4dfa377c1c1d2443d89c
Mike Morris <michael.a.morris.public@gmail.com>
2022-03-03 21:12:28 -0600

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.tsx
+++ src/pages/blog/{mdx.slug}.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 import type { PageProps } from 'gatsby'
+import type { ImageDataLike } from 'gatsby-plugin-image'
 
 import Layout from '../../components/Layout'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
@@ -10,14 +13,27 @@ type DataProps = {
     frontmatter: {
       date: string
       title: string
+      hero_image_alt: string
+      hero_image_credit_text: string
+      hero_image_credit_link: string
+      hero_image: ImageDataLike
     }
     body: string
   }
 }
 
 const BlogPost = ({ data }: PageProps<DataProps>) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>Posted: {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={image!} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{' '}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   )
@@ -29,6 +45,14 @@ export const query = graphql`
       frontmatter {
         date(formatString: "MMMM D, YYYY")
         title
+        hero_image_credit_text
+        hero_image_credit_link
+        hero_image_alt
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
